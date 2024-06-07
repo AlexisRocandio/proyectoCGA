256c986499970cba7e55fce05887233d4db0cb56
enrique <enriquepriego505@gmail.com>
2024-06-06 20:59:35 -0600

    update

---
+++ ShootInTheSpace/build/13-Particulas/CMakeFiles/13-Particulas.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/13-Particulas.dir/src/main.cpp.obj"
+  "CMakeFiles/13-Particulas.dir/src/main.cpp.obj.d"
+  "D:/PC/Documents/kiki/UNAM/GraficaAvanzada/proyecto2/proyectoCGA/ShootInTheSpace/bin/13-Particulas.exe"
+  "D:/PC/Documents/kiki/UNAM/GraficaAvanzada/proyecto2/proyectoCGA/ShootInTheSpace/bin/13-Particulas.exe.manifest"
+  "D:/PC/Documents/kiki/UNAM/GraficaAvanzada/proyecto2/proyectoCGA/ShootInTheSpace/bin/13-Particulas.pdb"
+  "lib13-Particulas.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/13-Particulas.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ ShootInTheSpace/build/CMakeFiles/CGALib.dir/cmake_clean.cmake
@@ -0,0 +1,19 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/CGALib.dir/src/Camera.obj"
+  "CMakeFiles/CGALib.dir/src/Camera.obj.d"
+  "CMakeFiles/CGALib.dir/src/Enemy.obj"
+  "CMakeFiles/CGALib.dir/src/Enemy.obj.d"
+  "CMakeFiles/CGALib.dir/src/Model.obj"
+  "CMakeFiles/CGALib.dir/src/Model.obj.d"
+  "CMakeFiles/CGALib.dir/src/main.obj"
+  "CMakeFiles/CGALib.dir/src/main.obj.d"
+  "D:/PC/Documents/kiki/UNAM/GraficaAvanzada/proyecto2/proyectoCGA/bin/libCGALib.dll"
+  "D:/PC/Documents/kiki/UNAM/GraficaAvanzada/proyecto2/proyectoCGA/bin/libCGALib.dll.manifest"
+  "D:/PC/Documents/kiki/UNAM/GraficaAvanzada/proyecto2/proyectoCGA/bin/libCGALib.pdb"
+  "libCGALib.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/CGALib.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
