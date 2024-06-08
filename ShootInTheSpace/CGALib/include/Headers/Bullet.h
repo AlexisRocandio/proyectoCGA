48826a4ba7975cc6c88a92bf2e9802d3330d17d4
enrique <enriquepriego505@gmail.com>
2024-06-08 01:12:14 -0600

    bullet

--- ShootInTheSpace/CGALib/include/Headers/Bullet.h
+++ ShootInTheSpace/CGALib/include/Headers/Bullet.h
@@ -1,3 +1,17 @@
+#include <string>
+#include <vector>
+#include <map>
+
+#include <assimp/Importer.hpp>
+#include <assimp/scene.h>
+#include <assimp/postprocess.h>
+
+// GL Includes
+#include <GL/glew.h>
+
+#include <glm/glm.hpp>
+#include <glm/gtc/matrix_transform.hpp>
+
 class Bullet {
 public:
     glm::vec3 position;
@@ -6,8 +20,10 @@ public:
     float speed;
     bool active;
 
-    Bullet(const glm::vec3 &pos, const glm::vec3 &dir, float spd)
-        : position(pos), initialPosition(pos), direction(dir), speed(spd), active(true) {}
+    AbstractModel bulletModel; // Modelo de la bala
+
+    Bullet(const glm::vec3 &pos, const glm::vec3 &dir, float spd, const AbstractModel &model)
+        : position(pos), initialPosition(pos), direction(dir), speed(spd), active(true), bulletModel(model) {}
 
     void update(float deltaTime) {
         if (active) {
@@ -16,6 +32,14 @@ public:
             if (glm::length(position - initialPosition) > 100.0f) {
                 active = false;
             }
+        }
+    }
+
+    void render() {
+        if (active) {
+            glm::mat4 modelMatrix = glm::mat4(1.0f);
+            modelMatrix = glm::translate(modelMatrix, position);
+            bulletModel.render(modelMatrix);
         }
     }
 };
